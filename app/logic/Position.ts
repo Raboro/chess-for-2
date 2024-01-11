067954722fe79aa01a4df5d28da4532834395582
Raboro <marius.woerfelreal@gmail.com>
2024-01-11 23:54:51 +0100

    added Position

---
+++ app/logic/Position.ts
@@ -0,0 +1,16 @@
+export default class Position {
+    readonly x: number;
+    readonly y: number;
+
+    constructor(x: number, y: number) {
+        if (x < 0 || x > 7 || y < 0 || y > 7) {
+            throw new RangeError("x or y get to valid input range [0,7]");
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    notSameLine(y: number): boolean {
+        return this.y != y;
+    }
+}
