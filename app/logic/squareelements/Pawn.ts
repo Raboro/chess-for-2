f389a36aade6275b9356342e273f7dcd11f1028f
Raboro <marius.woerfelreal@gmail.com>
2024-01-11 23:57:35 +0100

    added SquareElement Pawn

---
+++ app/logic/squareelements/Pawn.ts
@@ -0,0 +1,31 @@
+import Moveable from "../Moveable";
+import Position from "../Position";
+import SquareElement from "../SquareElement";
+import SquareElementType from "../SquareElementType";
+
+export default class Pawn implements SquareElement, Moveable {
+    position: Position;
+    squareElementType: Readonly<SquareElementType>;
+
+    constructor(position: Position, squareElementType: SquareElementType) {
+        this.position = position;
+        this.squareElementType = squareElementType;
+    }
+
+    isPiece(): boolean {
+        return true;
+    }
+
+    isMoveableTo(position: Position): boolean {
+        if (this.position.notSameLine(position.y)) {
+            return false;
+        }
+        return (this.squareElementType == 'black') ? this.position.y < position.y : this.position.y > position.y;
+    }
+
+    moveTo(position: Position): void {
+        if (this.isMoveableTo(position)) {
+            this.position = position;
+        }
+    }
+}
