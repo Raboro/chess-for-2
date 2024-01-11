c9de87f7e7d09540c3792df0dbfaa98412f217c6
Raboro <marius.woerfelreal@gmail.com>
2024-01-12 00:17:36 +0100

    added semicolon at the end of interface def

--- app/logic/Moveable.ts
+++ app/logic/Moveable.ts
@@ -3,4 +3,4 @@ import Position from "./Position";
 export default interface Moveable {
     isMoveableTo(position: Position): boolean,
     moveTo(position: Position): void
-}
+};
--- app/logic/SquareElement.ts
+++ app/logic/SquareElement.ts
@@ -5,4 +5,4 @@ export default interface SquareElement {
     position: Position,
     squareElementType: Readonly<SquareElementType>,
     isPiece(): boolean,
-}
+};
