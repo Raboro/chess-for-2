75c4195fe09555cba337cca6165bc4106d387e31
Raboro <marius.woerfelreal@gmail.com>
2024-01-17 21:47:30 +0100

    added valid moveable test for Queen

--- app/logic/squareelements/__tests__/Queen.test.ts
+++ app/logic/squareelements/__tests__/Queen.test.ts
@@ -19,6 +19,66 @@ describe('Queen', () => {
   );
 
   test.each([['White'], ['Black']])(
+    '%s Queen should be moveable - diagonal',
+    (color: string) => {
+      const queen = new Queen(
+        new Position(5, 3),
+        color === 'White' ? 'white' : 'black',
+      );
+      // 1 diagonal around
+      expect(queen.isMoveableTo(new Position(4, 2))).toBeTruthy();
+      expect(queen.isMoveableTo(new Position(6, 2))).toBeTruthy();
+      expect(queen.isMoveableTo(new Position(4, 4))).toBeTruthy();
+      expect(queen.isMoveableTo(new Position(6, 4))).toBeTruthy();
+
+      // 2 diagonal´s around
+      expect(queen.isMoveableTo(new Position(3, 1))).toBeTruthy();
+      expect(queen.isMoveableTo(new Position(7, 1))).toBeTruthy();
+      expect(queen.isMoveableTo(new Position(3, 5))).toBeTruthy();
+      expect(queen.isMoveableTo(new Position(7, 5))).toBeTruthy();
+
+      expect(queen.isMoveableTo(new Position(1, 7))).toBeTruthy();
+      expect(queen.isMoveableTo(new Position(2, 6))).toBeTruthy();
+    },
+  );
+
+  test.each([['White'], ['Black']])(
+    '%s Queen should be moveable on x axis',
+    (color: string) => {
+      for (let i = 0; i < 10; i++) {
+        const x = randomIntFromInterval(0, 7);
+        const y = randomIntFromInterval(0, 7);
+        const queen: Queen = new Queen(
+          new Position(x, y),
+          color == 'White' ? 'white' : 'black',
+        );
+
+        expect(
+          queen.isMoveableTo(new Position(x, getPositionNotEq(y))),
+        ).toBeTruthy();
+      }
+    },
+  );
+
+  test.each([['White'], ['Black']])(
+    '%s Queen should be moveable on y axis',
+    (color: string) => {
+      for (let i = 0; i < 10; i++) {
+        const x = randomIntFromInterval(0, 7);
+        const y = randomIntFromInterval(0, 7);
+        const queen: Queen = new Queen(
+          new Position(x, y),
+          color == 'White' ? 'white' : 'black',
+        );
+
+        expect(
+          queen.isMoveableTo(new Position(getPositionNotEq(x), y)),
+        ).toBeTruthy();
+      }
+    },
+  );
+
+  test.each([['White'], ['Black']])(
     '%s Queen move to valid',
     (color: string) => {
       const position = new Position(
@@ -52,4 +112,12 @@ describe('Queen', () => {
 
 const randomIntFromInterval = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min); //NOSONAR because random is okay here
+};
+
+const getPositionNotEq = (element: number): number => {
+  let newPosElement;
+  do {
+    newPosElement = randomIntFromInterval(0, 7);
+  } while (newPosElement === element);
+  return newPosElement;
 };
