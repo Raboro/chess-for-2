b903fd359fd1aa1089958467beca55f98f084296
Raboro <marius.woerfelreal@gmail.com>
2024-01-18 22:39:45 +0100

    extracted constant

--- app/components/Board/Board.tsx
+++ app/components/Board/Board.tsx
@@ -5,13 +5,15 @@ interface Props {
   size: number;
 }
 
+const LINE_SIZE = 8;
+
 const Board = (props: Readonly<Props>) => {
-  const squareSize: number = props.size / 8;
+  const squareSize: number = props.size / LINE_SIZE;
 
   const renderRow = (index: number) => {
     return (
       <View key={`row-${index}`} style={{ flexDirection: 'row' }}>
-        {Array.from({ length: 8 }, (_, i) => (
+        {Array.from({ length: LINE_SIZE }, (_, i) => (
           <Square
             key={`square-${index}-${i}`}
             size={squareSize}
@@ -24,7 +26,7 @@ const Board = (props: Readonly<Props>) => {
 
   return (
     <View style={{ width: props.size, height: props.size }}>
-      {[...Array(8)].map((_, i) => renderRow(i))}
+      {[...Array(LINE_SIZE)].map((_, i) => renderRow(i))}
     </View>
   );
 };
