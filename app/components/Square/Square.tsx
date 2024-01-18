3b4ce61f7c06e4b5ed94bc0a40b20fa46caa6897
Raboro <marius.woerfelreal@gmail.com>
2024-01-18 22:32:57 +0100

    added Square with width/height and bgcolor

---
+++ app/components/Square/Square.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import SquareType from '../../constants/SquareType';
+import { TouchableOpacity } from 'react-native';
+
+interface Props {
+  size: number;
+  squareType: SquareType;
+}
+
+const Square = (props: Readonly<Props>) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: props.size,
+        height: props.size,
+        backgroundColor: props.squareType,
+      }}
+    ></TouchableOpacity>
+  );
+};
+
+export default Square;
