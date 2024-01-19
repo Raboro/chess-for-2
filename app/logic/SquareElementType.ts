55df3e0a6808f5c311dae26ef7120d7ca7720954
Raboro <marius.woerfelreal@gmail.com>
2024-01-19 23:24:49 +0100

    added isWhite function to reduce duplicated code

--- app/logic/SquareElementType.ts
+++ app/logic/SquareElementType.ts
@@ -1,3 +1,7 @@
 type SquareElementType = 'white' | 'black' | undefined;
 
+export function isWhite(squareElementType: SquareElementType): boolean {
+  return squareElementType === 'white';
+}
+
 export default SquareElementType;
