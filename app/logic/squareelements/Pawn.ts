2d7384fb1bb983df1b04387b6952838d89057780
Raboro <marius.woerfelreal@gmail.com>
2024-01-23 22:25:41 +0100

    made it possible that pawn can move one diagonal right/left

--- app/logic/squareelements/Pawn.ts
+++ app/logic/squareelements/Pawn.ts
@@ -12,23 +12,47 @@ export default class Pawn extends Piece implements Moveable, Displayable {
   }
 
   isMoveableTo(position: Position): boolean {
-    if (this.position.notSameLine(position) || this.position.same(position)) {
+    if (this.position.same(position)) {
       return false;
     }
 
-    if (this.squareElementType == 'black') {
-      if (this.position.y >= position.y) {
-        // not behind pawn
-        return false;
-      }
-      return this.position.y == 1
-        ? this.position.y + 2 >= position.y
-        : this.position.y + 1 == position.y;
+    return this.squareElementType == 'black'
+      ? this.isMoveableToBlack(position)
+      : this.isMoveableToWhite(position);
+  }
+
+  private isMoveableToBlack(position: Position): boolean {
+    if (this.isOneDiagonalUp(position, 1)) {
+      return true;
     }
-    if (this.position.y <= position.y) {
-      // not behind pawn
+
+    // need to be checked after, because else oneDiagonal could never be true
+    if (this.position.y >= position.y || this.position.notSameLine(position)) {
       return false;
     }
+
+    return this.position.y == 1
+      ? this.position.y + 2 >= position.y
+      : this.position.y + 1 == position.y;
+  }
+
+  private isOneDiagonalUp(position: Position, addFactor: number): boolean {
+    return (
+      this.position.y + addFactor === position.y &&
+      this.position.differenceOfOneX(position)
+    );
+  }
+
+  private isMoveableToWhite(position: Position): boolean {
+    if (this.isOneDiagonalUp(position, -1)) {
+      return true;
+    }
+
+    // need to be checked after, because else oneDiagonal could never be true
+    if (this.position.y <= position.y || this.position.notSameLine(position)) {
+      return false;
+    }
+
     return this.position.y == 6
       ? this.position.y - 2 <= position.y
       : this.position.y - 1 == position.y;
