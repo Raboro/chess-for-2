2e8a6c92b627bebaecae9b2e995a469e8f78de99
Raboro <marius.woerfelreal@gmail.com>
2024-01-24 23:30:17 +0100

    implemented StraightPathConstructor construction of path & added tests for it

--- app/logic/path/StraightPathConstructor.ts
+++ app/logic/path/StraightPathConstructor.ts
@@ -5,8 +5,30 @@ import Position from '../Position';
 export default class StraightPathConstructor implements PathConstructor {
   construct(current: Position, destination: Position): Path {
     const path: Path = new Path();
-    path.add(current);
-    path.add(destination);
+    let moveX;
+    let moveY;
+    let necessaryMoves;
+
+    if (current.x === destination.x) {
+      moveX = 0;
+      moveY = destination.y < current.y ? -1 : 1;
+      necessaryMoves =
+        destination.y < current.y
+          ? current.y - destination.y
+          : destination.y - current.y;
+    } else {
+      moveX = destination.x < current.x ? -1 : 1;
+      moveY = 0;
+      necessaryMoves =
+        destination.x < current.x
+          ? current.x - destination.x
+          : destination.x - current.x;
+    }
+
+    for (let i = 1; i < necessaryMoves; i++) {
+      path.add(new Position(current.x + moveX * i, current.y + moveY * i));
+    }
+
     return path;
   }
 }
---
+++ app/logic/path/__tests__/StraightPathConstructor.test.ts
@@ -0,0 +1,80 @@
+import { describe, expect, test } from '@jest/globals';
+import StraightPathConstructor from '../StraightPathConstructor';
+import Position from '../../Position';
+import Path from '../Path';
+
+describe('StraightPathConstructor', () => {
+  const pathConstructor = new StraightPathConstructor();
+
+  test('Move to same position should be empty path', () => {
+    const position: Position = new Position(3, 5);
+    const path: Path = pathConstructor.construct(position, position);
+
+    let counter = 0;
+    for (let _ of path) {
+      console.log(_);
+      counter++;
+    }
+
+    expect(counter).toEqual(0);
+  });
+
+  test('Move on X-aches left', () => {
+    const result: Position[] = [new Position(2, 5), new Position(1, 5)];
+    const path: Path = pathConstructor.construct(
+      new Position(3, 5),
+      new Position(0, 5),
+    );
+
+    let index = 0;
+    for (const element of path) {
+      expect(element).toEqual(result[index++]);
+    }
+  });
+
+  test('Move on X-aches right', () => {
+    const result: Position[] = [
+      new Position(4, 5),
+      new Position(5, 5),
+      new Position(6, 5),
+    ];
+    const path: Path = pathConstructor.construct(
+      new Position(3, 5),
+      new Position(7, 5),
+    );
+    let index = 0;
+    for (const element of path) {
+      expect(element).toEqual(result[index++]);
+    }
+  });
+
+  test('Move on Y-aches down', () => {
+    const result: Position[] = [new Position(5, 2), new Position(5, 1)];
+    const path: Path = pathConstructor.construct(
+      new Position(5, 3),
+      new Position(5, 0),
+    );
+
+    let index = 0;
+    for (const element of path) {
+      expect(element).toEqual(result[index++]);
+    }
+  });
+
+  test('Move on Y-aches up', () => {
+    const result: Position[] = [
+      new Position(5, 4),
+      new Position(5, 5),
+      new Position(5, 6),
+    ];
+    const path: Path = pathConstructor.construct(
+      new Position(5, 3),
+      new Position(5, 7),
+    );
+
+    let index = 0;
+    for (const element of path) {
+      expect(element).toEqual(result[index++]);
+    }
+  });
+});
