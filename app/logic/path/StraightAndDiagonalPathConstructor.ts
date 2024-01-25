feeec5bd07dd2b176496837f75928bb0a0657347
Raboro <marius.woerfelreal@gmail.com>
2024-01-25 22:14:22 +0100

    implemented StraightAndDiagonalPathConstructor

--- app/logic/path/StraightAndDiagonalPathConstructor.ts
+++ app/logic/path/StraightAndDiagonalPathConstructor.ts
@@ -1,11 +1,20 @@
 import Position from '../Position';
+import DiagonalPathConstructor from './DiagonalPathConstructor';
 import Path from './Path';
 import PathConstructor from './PathConstructor';
+import StraightPathConstructor from './StraightPathConstructor';
 
 export default class StraightAndDiagonalPathConstructor
   implements PathConstructor
 {
   construct(current: Position, destination: Position): Path {
-    return new Path();
+    const pathConstructor = this.oneSame(current, destination)
+      ? new StraightPathConstructor()
+      : new DiagonalPathConstructor();
+    return pathConstructor.construct(current, destination);
+  }
+
+  private oneSame(current: Position, destination: Position): boolean {
+    return current.x === destination.x || current.y === destination.y;
   }
 }
