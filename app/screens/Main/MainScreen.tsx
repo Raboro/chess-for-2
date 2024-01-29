1dbdb833c5be73a9900acb128a7915da3f888098
Raboro <marius.woerfelreal@gmail.com>
2024-01-29 23:01:29 +0100

    included promotion for black and white

--- app/screens/Main/MainScreen.tsx
+++ app/screens/Main/MainScreen.tsx
@@ -4,14 +4,22 @@ import Board from '../../components/Board/Board';
 
 import { Board as BoardLogic } from '../../logic/Board';
 import { styles } from './MainScreenStyle';
+import Promotion from '../../components/Promotion/Promotion';
+import SquareColor from '../../constants/SquareColor';
+import { useState } from 'react';
+import SquareElementType from '../../logic/SquareElementType';
 
 const MainScreen = () => {
+  const [promotion, setPromotion] = useState<SquareElementType>(undefined);
+
   return (
     <SafeAreaView style={styles.container}>
+      {promotion === 'white' && <Promotion size={Dimensions.get('screen').width} squareColor={SquareColor.BLACK} squareElementType='black'/>}
       <Board
         size={Dimensions.get('screen').width}
         boardLogic={new BoardLogic()}
       />
+      {promotion === 'black' && <Promotion size={Dimensions.get('screen').width} squareColor={SquareColor.WHITE} squareElementType='white'/>}
     </SafeAreaView>
   );
 };
