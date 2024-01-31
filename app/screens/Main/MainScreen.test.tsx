d7aca5f119b4fe3715fee41d815835ea1f46f176
Raboro <marius.woerfelreal@gmail.com>
2024-01-31 23:46:33 +0100

    added Test for MainScreen to promote white

--- app/screens/Main/MainScreen.test.tsx
+++ app/screens/Main/MainScreen.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 import { NavigationContainer } from '@react-navigation/native';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import React from 'react';
 import MainScreen from './MainScreen';
 
@@ -13,4 +13,52 @@ describe('MainScreen', () => {
     );
     expect(rend).not.toBe(null);
   });
+
+  test('Should trigger Promotion for white', () => {
+    const rend = render(
+      <NavigationContainer>
+        <MainScreen />
+      </NavigationContainer>,
+    );
+    
+    const whitePositions = [
+      [55, 47],
+      [47, 39],
+      [39, 31],
+      [31, 23],
+      [23, 14],
+    ];
+    const blackPositions = [
+      [8, 16],
+      [16, 24],
+      [24, 32],
+      [32, 40],
+      [9, 17],
+    ];
+
+    let whitePawn;
+    let whiteMoveSquare;
+    let blackPawn;
+    let blackMoveSquare;
+    for (let i = 0; i < 5; i++) {
+      whitePawn = rend.getAllByTestId('SquareImage')[whitePositions[i][0]];
+      fireEvent(whitePawn, 'press');
+      whiteMoveSquare =
+        rend.getAllByTestId('SquareImage')[whitePositions[i][1]];
+      fireEvent(whiteMoveSquare, 'press');
+
+      blackPawn = rend.getAllByTestId('SquareImage')[blackPositions[i][0]];
+      fireEvent(blackPawn, 'press');
+      blackMoveSquare =
+        rend.getAllByTestId('SquareImage')[blackPositions[i][1]];
+      fireEvent(blackMoveSquare, 'press');
+    }
+
+    whitePawn = rend.getAllByTestId('SquareImage')[14];
+    fireEvent(whitePawn, 'press');
+    whiteMoveSquare = rend.getAllByTestId('SquareImage')[5];
+    fireEvent(whiteMoveSquare, 'press');
+
+    fireEvent(rend.getAllByTestId('Square')[67], 'press')
+  })
 });
