2cb17fc47bae81ae6cd132f50cab737eee26eec4
Raboro <marius.woerfelreal@gmail.com>
2024-02-01 23:39:24 +0100

    added Test for MainScreen to promote black

--- app/screens/Main/MainScreen.test.tsx
+++ app/screens/Main/MainScreen.test.tsx
@@ -61,4 +61,49 @@ describe('MainScreen', () => {
 
     fireEvent(rend.getAllByTestId('Square')[67], 'press');
   });
+
+  test('Should trigger Promotion for black', () => {
+    const rend = render(
+      <NavigationContainer>
+        <MainScreen />
+      </NavigationContainer>,
+    );
+
+    const whitePositions = [
+      [55, 47],
+      [47, 39],
+      [39, 31],
+      [31, 23],
+      [54, 46],
+      [46, 38],
+    ];
+    const blackPositions = [
+      [8, 16],
+      [16, 24],
+      [24, 32],
+      [32, 40],
+      [40, 49],
+      [49, 58],
+    ];
+
+    let whitePawn;
+    let whiteMoveSquare;
+    let blackPawn;
+    let blackMoveSquare;
+    for (let i = 0; i < 6; i++) {
+      whitePawn = rend.getAllByTestId('SquareImage')[whitePositions[i][0]];
+      fireEvent(whitePawn, 'press');
+      whiteMoveSquare =
+        rend.getAllByTestId('SquareImage')[whitePositions[i][1]];
+      fireEvent(whiteMoveSquare, 'press');
+
+      blackPawn = rend.getAllByTestId('SquareImage')[blackPositions[i][0]];
+      fireEvent(blackPawn, 'press');
+      blackMoveSquare =
+        rend.getAllByTestId('SquareImage')[blackPositions[i][1]];
+      fireEvent(blackMoveSquare, 'press');
+    }
+
+    fireEvent(rend.getAllByTestId('Square')[67], 'press');
+  });
 });
