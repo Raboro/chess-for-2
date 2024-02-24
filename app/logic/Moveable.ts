5c711f7b54e736250330b298cb1cd2a8f6e270dc
Raboro <raborogit@gmail.com>
2024-02-24 21:14:15 +0100

    added doc to Movable interface

--- app/logic/Moveable.ts
+++ app/logic/Moveable.ts
@@ -1,6 +1,15 @@
 import Position from './Position';
 
 export default interface Moveable {
+  /**
+   * Is moveable to parsed position
+   * @param position
+   */
   isMoveableTo(position: Position): boolean;
+
+  /**
+   * Move Piece to parsed position
+   * @param position
+   */
   moveTo(position: Position): void;
 } // eslint-disable-line
