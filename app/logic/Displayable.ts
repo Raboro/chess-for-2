66728a9048b59a332262369dcefeb16d944de7ab
Raboro <marius.woerfelreal@gmail.com>
2024-02-26 22:03:20 +0100

    added comment to explain method

--- app/logic/Displayable.ts
+++ app/logic/Displayable.ts
@@ -1,5 +1,8 @@
 import { ImageSourcePropType } from 'react-native';
 
 export default interface Displayable {
+  /**
+   * used to render Piece on the board by returning the image of the Piece
+   */
   display(): ImageSourcePropType;
 } // eslint-disable-line
